ae2a4a9652eb6ef5ab0d82c7073740bfc4be76f9
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2022-06-16 08:10:56 +0300

    delete folder

--- sql requests/auto increment reset.sql
+++
@@ -1,2 +0,0 @@
-/* in this project i use ClearDB (Heroku - sql) */
-SET @@auto_increment_increment=1;
--- sql requests/select_tour-title_pl-name.sql
+++
@@ -1,6 +0,0 @@
-SELECT tour.title, pl.name
-FROM player_tournament pltour
-INNER JOIN tournaments tour ON pltour.tournament_id=tour.id 
-INNER JOIN players pl ON pltour.player_id=pl.id;
-
-
